b47a76a064288139cf85b2323c51496ad51e4fa2
Skies01 <135945944+Skies01@users.noreply.github.com>
2026-05-04 23:53:39 +0100

    Update approve.js

--- public/api/approve.js
+++ public/api/approve.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const { paymentId } = req.query;
-  const API_KEY = '2tybt4fvkhcecdluhktnqa5rkj6iddetvtojk439l86pnxmw2wny88ytq6mhabqz
+  const API_KEY = '2tybt4fvkhcecdluhktnqa5rkj6iddetvto-jk439l86pnxmw2wny88ytq6mhabqz
 '; // From Pi Dev Step 6
   
   await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
