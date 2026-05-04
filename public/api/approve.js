c4701d3e4cafac0a5a548742de15570a65af8e4b
Skies01 <135945944+Skies01@users.noreply.github.com>
2026-05-04 23:50:24 +0100

    Add payment approval API handler

---
+++ public/api/approve.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const { paymentId } = req.query;
+  const API_KEY = '2tybt4fvkhcecdluhktnqa5rkj6iddetvtojk439l86pnxmw2wny88ytq6mhabqz
+'; // From Pi Dev Step 6
+  
+  await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
+    method: 'POST',
+    headers: { 'Authorization': `Key ${API_KEY}` }
+  });
+  
+  res.status(200).json({ message: 'Approved' });
+}
